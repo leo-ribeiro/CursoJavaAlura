75319b5ce3a93317d9db3720ca2c047798711a55
leopempem <leopemribeiro@gmail.com>
2022-09-21 18:17:34 -0300

    Métodos

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -8,7 +8,25 @@ public class Conta {
 	String titular;
 	
 	public void deposita(double valor) {
-		this.saldo = this.saldo + valor;
-		
+		this.saldo = this.saldo + valor;	// O jeito mais comum de se usar é assim: this.saldo = += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {   
+			this.saldo = this.saldo -  valor; // O jeito mais comum de se usar é assim: if(this.saldo += valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -7,6 +7,18 @@ public class TestaMetodo {
 		contaDoLeonardo.saldo = 100;
 		contaDoLeonardo.deposita(50);
 		System.out.println(contaDoLeonardo.saldo);
+		
+		boolean conseguiuRetirar = contaDoLeonardo.saca(20);
+		System.out.println(contaDoLeonardo.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		contaDaMarcela.transfere(300, contaDoLeonardo);
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoLeonardo.saldo);
 	}
-
+	
+		
 }
