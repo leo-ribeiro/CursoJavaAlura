2783cc96e699b2f4230b9cded3335389eb90eda4
leopempem <leopemribeiro@gmail.com>
2022-09-21 08:38:05 -0300

    Multiplos de Tres

--- sintaxe-variaveis-e-fluxo/src/MultiplosDeTres.java
+++ sintaxe-variaveis-e-fluxo/src/MultiplosDeTres.java
@@ -5,7 +5,7 @@ public class MultiplosDeTres {
 		
 		for(int numero = 1; numero < 100; numero++) {
 			if (numero % 3 == 0)
-				System.out.println(numero);
+				System.out.print(numero + ", ");
 	
 		}
 	}
--- sintaxe-variaveis-e-fluxo/src/MultiplosDeTresAteCem.java
+++ sintaxe-variaveis-e-fluxo/src/MultiplosDeTresAteCem.java
@@ -1,4 +1,9 @@
 
 public class MultiplosDeTresAteCem {
 
+	public static void main(String[] args) {
+		for (int numero = 3; numero < 100; numero += 3) {
+			System.out.println(numero);
+		}
+	}
 }
