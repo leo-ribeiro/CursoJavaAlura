756f8141a425b01327644d59942464321aa9cca2
leopempem <leopemribeiro@gmail.com>
2022-09-23 18:29:20 -0300

    Exercício Apostila

---
+++ sintaxe-variaveis-e-fluxo/src/ExercicioVariaveisEPrimitivos.java
@@ -0,0 +1,14 @@
+
+public class ExercicioVariaveisEPrimitivos {
+	
+	public static void main(String[] args) {
+		
+		double janeiro = 15.000;
+		double fevereiro = 23.000;
+		double marco = 17.000;
+		double total = (janeiro + fevereiro + marco);
+		
+		System.out.println("A despesa total do primeiro trimestre, é de : R$ " + total);
+	}
+
+}
