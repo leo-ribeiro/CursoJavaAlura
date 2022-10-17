b31c8081b8e350b0d107672efdab3c5caf874c7f
leopempem <leopemribeiro@gmail.com>
2022-10-17 15:48:59 -0300

    FQN

--- bytebank-herdado-conta/src/com/br/bytebank/banco/teste/TesteSaca.java
+++ bytebank-herdado-conta/src/com/br/bytebank/banco/teste/TesteSaca.java
@@ -1,9 +1,11 @@
-package teste;
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo*;
 
 public class TesteSaca {
 
 	public static void main(String[] args) {
-		ContaCorrente conta = new ContaCorrente(123, 321);
+		modelo.ContaCorrente conta = new modelo.ContaCorrente(123, 321);
 		
 		conta.deposita(200.0);
 		try {
--- bytebank-herdado-conta/src/com/br/bytebank/banco/teste/testeContas.java
+++ bytebank-herdado-conta/src/com/br/bytebank/banco/teste/testeContas.java
@@ -1,13 +1,15 @@
-package teste;
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo*;
 
 public class testeContas {
 
 	public static void main(String[] args) throws modelo.SaldoInsuficienteException {
 		
-		modelo.ContaCorrente cc = new modelo.ContaCorrente(111, 111);
+		ContaCorrente cc = new ContaCorrente(111, 111);
 		cc.deposita(100.0);
 		
-		modelo.ContaPoupanca cp = new modelo.ContaPoupanca(222, 222);
+		ContaPoupanca cp = new ContaPoupanca(222, 222);
 		cp.deposita(200.0);
 		
 		cc.transfere(10.0, cp);
