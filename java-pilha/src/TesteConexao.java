2cbad899edea5fde2020db1bce0524e7e720c5d7
leopempem <leopemribeiro@gmail.com>
2022-10-17 09:20:56 -0300

    Finally

--- java-pilha/src/TesteConexao.java
+++ java-pilha/src/TesteConexao.java
@@ -3,15 +3,27 @@ public class TesteConexao {
 
 	public static void main(String[] args) {
 		
-		Conexao con = null;
-		try {
-			con = new Conexao();
-			con.leDados();
+		try (Conexao conexao = new Conexao() ) {
+			conexao.leDados();
 		} catch(IllegalStateException ex) {
-			System.out.println("Deu erro na conexão");			
-		} finally {
-			con.fecha();
+			System.out.println("Deu erro na conexão");
 		}
+		
+		
+		//-------------------------------------------------
+		
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//		} catch(IllegalStateException ex) {
+//			System.out.println("Deu erro na conexão");			
+//		} finally {
+//			System.out.println("Finally");
+//			if(con != null) {
+//				con.close();
+//			}
+//		}
 
 	}
 
